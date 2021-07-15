5f99340d7539cdc052ef033d4ac62e6b365f8fbf
Lucio <lesolari_13@hotmail.com>
2021-07-15 15:02:20 -0300

    fix practicas????

--- src/validators/practicas/busPractica.validator.ts
+++
@@ -1,7 +0,0 @@
-import { IsString } from 'class-validator';
-export class BusPractica {
-  @IsString({
-    message: 'Practica debe ser un string',
-  })
-  practica: string;
-}
--- src/validators/practicas/practicasid.validator.ts
+++
@@ -1,7 +0,0 @@
-import { IsString } from 'class-validator';
-export class BusPracticaId {
-  @IsString({
-    message: 'Practica debe ser un String',
-  })
-  practica: string;
-}
