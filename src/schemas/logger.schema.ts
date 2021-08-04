24e1734475c99ef8a0d60dba6aed33a875b3ffe0
Lucio <lesolari_13@hotmail.com>
2021-08-04 12:11:35 -0300

    fix logger

--- src/schemas/logger.schema.ts
+++ src/schemas/logger.schema.ts
@@ -1,41 +1,40 @@
-import * as mongoose from "mongoose";
+import * as mongoose from 'mongoose';
 
-const auto = new mongoose.Schema(
-  {
-    body: {
-      type: String,
-    },
-    params: {
-      type: String,
-    },
-    query: {
-      type: String,
-    },
-    response: {
-      type: String,
-    },
-    ip: {
-      type: String,
-      required: true,
-    },
-    client: {
-      type: String,
-      required: true,
-    },
-    url: {
-      type: String,
-      required: true,
-    },
-    method: {
-      type: String,
-      required: true,
-    },
-    user: {
-      type: String,
-      required: true,
-    },
+const auto = new mongoose.Schema({
+  body: {
+    type: String,
   },
-  { timestamps: { createdAt: "date" } }
+  params: {
+    type: String,
+  },
+  query: {
+    type: String,
+  },
+  response: {
+    type: String,
+  },
+  ip: {
+    type: String,
+    required: true,
+  },
+  client: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  method: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: String,
+    required: true,
+  },
+},
+{ timestamps: { createdAt: "created" } }
 );
 
 export const Logger = auto;
