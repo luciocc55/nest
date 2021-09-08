d356c80e220b546e1ff0bc6f739bf8f3213661b9
Lucio <lesolari_13@hotmail.com>
2021-09-08 11:11:05 -0300

    fix logger con errores

--- src/interceptors/logger/logger.interceptor.ts
+++ src/interceptors/logger/logger.interceptor.ts
@@ -1,28 +1,41 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
-import { LoggerService } from 'src/services/logger/logger.service';
-import { AuthService } from 'src/services/auth/auth.service';
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from "@nestjs/common";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+import { LoggerService } from "src/services/logger/logger.service";
+import { AuthService } from "src/services/auth/auth.service";
 export interface Response<T> {
   data: T;
 }
 @Injectable()
 export class LoggingInterceptor<T> implements NestInterceptor<T, Response<T>> {
-  constructor(private logService: LoggerService, private authService: AuthService ) {}
-  async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<Response<T>>> {
+  constructor(
+    private logService: LoggerService,
+    private authService: AuthService
+  ) {}
+  async intercept(
+    context: ExecutionContext,
+    next: CallHandler
+  ): Promise<Observable<Response<T>>> {
     const contexto = context.getArgs();
     // tslint:disable-next-line: one-variable-per-declaration
-    const user = await this.authService.decode(contexto[0].headers.authorization);
+    const user = await this.authService.decode(
+      contexto[0].headers.authorization
+    );
     const method = contexto[0].method;
     const url = contexto[0].url;
-    let body = '';
-    if (!url.includes('/orono/usuarios/')) {
+    let body = "";
+    if (!url.includes("/orono/usuarios/")) {
       body = JSON.stringify(contexto[0].body);
     }
     const params = JSON.stringify(contexto[0].params);
     const query = JSON.stringify(contexto[0].query);
-    const ip = contexto[0].header('x-forwarded-for') || contexto[0].ip;
-    const client = contexto[0].header('user-agent');
+    const ip = contexto[0].header("x-forwarded-for") || contexto[0].ip;
+    const client = contexto[0].header("user-agent");
     const log = {
       method,
       url,
@@ -30,14 +43,27 @@ export class LoggingInterceptor<T> implements NestInterceptor<T, Response<T>> {
       query,
       body,
       ip,
-      user: user['user'],
+      user: user["user"],
       client,
     };
     const logRegistro = await this.logService.create(log);
     const request = context.switchToHttp().getRequest();
     request.params.IdTransaccion = logRegistro['_id']
-    return next.handle().pipe(tap((async data => {
-      this.logService.writeResponse(JSON.stringify(data), logRegistro['_id']);
-    })));
+    return next.handle().pipe(
+      tap(
+        (data) => {
+          this.logService.writeResponse(
+            JSON.stringify(data),
+            logRegistro["_id"]
+          );
+        },
+        (err) => {
+          this.logService.writeResponse(
+            JSON.stringify(err),
+            logRegistro["_id"]
+          );
+        }
+      )
+    );
   }
 }
