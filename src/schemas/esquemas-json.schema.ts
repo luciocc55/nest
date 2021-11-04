9203cc5ad224d67cd0e01b119ab425cc8f07d90e
Lucio <lesolari_13@hotmail.com>
2021-11-04 11:49:25 -0300

    Remove index

--- src/schemas/esquemas-json.schema.ts
+++ src/schemas/esquemas-json.schema.ts
@@ -38,8 +38,5 @@ const auto = new mongoose.Schema(
   },
   { timestamps: {} }
 );
-auto.index(
-  { origen: 1, user: 1, servicio: 1, sinonimoOrigen: 1 },
-  { unique: true }
-);
+
 export const Esquemas = auto;
